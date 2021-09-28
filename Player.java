3ac1e60bf979fd0947cc7466872926e76d1932b7
Jackson-Harrison <jh02160@srcs.org>
2021-09-28 15:22:39 -0700

    Halfway Through 4 (9/28/20210

--- Player.java
+++ Player.java
@@ -8,12 +8,53 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Player extends Actor
 {
-    /**
-     * Act - do whatever the Player wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
+    private Health[] health;
+    private PowerUp[] powerup;
+    private int healthCount;
+    private int spped;
+    private int walkIndex;
+    private int frame;
+    private float yVelocity;
+    private boolean isWalking;
+    private boolean isJumping;
+    private boolean isFacingLeft;
+    private final GreenfootImage[] WALK_ANIMATION;
+    private final GreenfootImage STANDING_IMAGE;
+    private final float GRAVITY;
+    private final float JUMP_FORCE;
+    private final Class NEXT_LEVEL;
+    private final GreenfootSound MUSIC;
+    
+    public Player(int speed, float jumpforce, float gravity, int maxHealth,
+    int maxPowerUp, Class nextLevel, GreenfootSound music)
+    {
+        WALK_ANIMATION = new GreenfootImage[]
+                         {
+                             new GreenfootImage("walk0.png"),
+                             new GreenfootImage("walk1.png"),
+                             new GreenfootImage("walk2.png"),
+                             new GreenfootImage("walk3.png"),
+                             new GreenfootImage("walk4.png"),
+                             new GreenfootImage("walk5.png"),
+                         };
+    }
+    
+                  
+    
     public void act()
     {
-        // Add your action code here.
+    }
+    public void addedToWorld(World world) {}
+    
+    private void walk() {}
+    private void jump() {}
+    private void fall() {}
+    private void animator() {}
+    private void onCollision() {}
+    private void mirrorImages() {}
+    private void gameOver() {}
+    private boolean isOnGround() 
+    {
+        
     }
 }
