571d93bb03d759266118584ed90ab6ca4e18c7a0
Jackson-Harrison <89489513+Jackson-Harrison@users.noreply.github.com>
2021-10-05 14:58:28 -0700

    Sam Activity Working animation
    Finally fixed the Music Issue

--- Level1.java
+++ Level1.java
@@ -9,6 +9,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class Level1 extends World
 {
     private final float GRAVITY = 0.0007f;
+    private final GreenfootSound MUSIC = new GreenfootSound("zapsplat_024.mp3");
     /**
      * Constructor for objects of class BrickWorld.
      * 
@@ -35,9 +36,9 @@ public class Level1 extends World
         addObject(door,754,248);
         removeObject(door);
         addObject(door,1176,40);
-        Player player = new Player(3, 5.6f, GRAVITY, 3, 3, Level2.class, player.Music);
+        Player player = new Player(3, 5.6f, GRAVITY, 3, 3, Level2.class, MUSIC);
         addObject(player,25,671);
-        addObject(new SmBrickWall(), 175, 100);
+        addObject(new SmBrickWall(), 175, 100); 
         addObject(new SmBrickWall(), 125, 500);
         addObject(new SmBrickWall(), 600, 150);
         addObject(new SmBrickWall(), 1100, 150);
